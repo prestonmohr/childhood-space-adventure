5de0d0fa36702ede39f3d19c28f6869f7b57b636
Preston Mohr <77995599+prestonmohr@users.noreply.github.com>
2023-02-10 13:12:14 -0500

    Update grain.js

--- grain.js
+++ grain.js
@@ -1,1 +1,45 @@
-const grainCanvas = document.querySelector("#grain");const grainCtx = grainCanvas.getContext("2d");const grainSize = 70;function createGrainTexture() {  const grainTextureCanvas = document.createElement("canvas");  grainTextureCanvas.width = grainSize * 2;  grainTextureCanvas.height = grainSize * 2;  const grainTextureCtx = grainTextureCanvas.getContext("2d");  for (let i = 0; i < grainSize * 2; i++) {    for (let j = 0; j < grainSize * 2; j++) {      const randomOpacity = Math.random() * 0.05;      grainTextureCtx.fillStyle = `rgba(255, 255, 255, ${randomOpacity})`;      grainTextureCtx.fillRect(i, j, 1, 1);    }  }  return grainTextureCanvas;}const grainTexture = createGrainTexture();function onResize() {  grainCanvas.width = window.innerWidth;  grainCanvas.height = window.innerHeight;}function animateGrain() {  grainCtx.clearRect(0, 0, grainCanvas.width, grainCanvas.height);  grainCtx.globalCompositeOperation = "multiply";  for (let i = 0; i < grainCanvas.width; i += grainSize * 2) {    for (let j = 0; j < grainCanvas.height; j += grainSize * 2) {      grainCtx.drawImage(grainTexture, i, j, grainSize * 2, grainSize * 2);    }  }  grainCtx.globalCompositeOperation = "source-over";  requestAnimationFrame(animateGrain);}window.addEventListener("resize", onResize);onResize();animateGrain();
+const grainCanvas = document.querySelector("#grain");
+const grainCtx = grainCanvas.getContext("2d");
+const grainSize = 70;
+
+function createGrainTexture() {
+  const grainTextureCanvas = document.createElement("canvas");
+  grainTextureCanvas.width = grainSize * 2;
+  grainTextureCanvas.height = grainSize * 2;
+  const grainTextureCtx = grainTextureCanvas.getContext("2d");
+
+  for (let i = 0; i < grainSize * 2; i++) {
+    for (let j = 0; j < grainSize * 2; j++) {
+      const randomOpacity = Math.random() * 0.05;
+      grainTextureCtx.fillStyle = `rgba(255, 255, 255, ${randomOpacity})`;
+      grainTextureCtx.fillRect(i, j, 1, 1);
+    }
+  }
+
+  return grainTextureCanvas;
+}
+
+const grainTexture = createGrainTexture();
+
+function onResize() {
+  grainCanvas.width = window.innerWidth;
+  grainCanvas.height = window.innerHeight;
+}
+
+function animateGrain() {
+  grainCtx.clearRect(0, 0, grainCanvas.width, grainCanvas.height);
+  grainCtx.globalCompositeOperation = "multiply";
+
+  for (let i = 0; i < grainCanvas.width; i += grainSize * 2) {
+    for (let j = 0; j < grainCanvas.height; j += grainSize * 2) {
+      grainCtx.drawImage(grainTexture, i, j, grainSize * 2, grainSize * 2);
+    }
+  }
+
+  grainCtx.globalCompositeOperation = "source-over";
+  requestAnimationFrame(animateGrain);
+}
+
+window.addEventListener("resize", onResize);
+onResize();
+animateGrain();
